cc175d72eaf69319d540c3c390f9683cbc90c5e8
Philip Alegret <philip.alegret@gmail.com>
2024-02-13 14:15:12 -0500

    Initial commit, includes zoom animation for burrito.

---
+++ taco-spinner/src/js/app.js
@@ -0,0 +1,9 @@
+const tacoBtn = document.getElementById('taco-btn')
+tacoBtn.addEventListener('click', () => {
+  tacoBtn.classList.toggle('spin')
+})
+
+const burritoBtn = document.getElementById('burrito-btn')
+burritoBtn.addEventListener('click', () => {
+  burritoBtn.classList.toggle('spin')
+})
